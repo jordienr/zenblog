5523cd3a2e29f72c09ecea01638a6bae3d97910f
Jordi <jordi.err@gmail.com>
2023-07-16 13:53:53 +0200

    rm debug url

--- apps/tailwindutils/zendo.ts
+++ apps/tailwindutils/zendo.ts
@@ -2,7 +2,7 @@ import { createClient } from "@zendo/cms";
 
 export function getCMS() {
   const cms = createClient({
-    _url: "http://localhost:3001/api/public",
+    // _url: "http://localhost:3001/api/public",
     blogId: "eb37085e-2220-4404-bd07-ad2848094fa8",
     debug: true,
   });
