b14ecd6b71e3ed328c70473cfa8ed7ae63513a18
Jordi Enric <jordi.err@gmail.com>
2023-10-07 11:31:27 +0200

    rename zendoblog to zendo

--- apps/zendoblog/src/components/Spinner.tsx
+++
@@ -1,7 +0,0 @@
-export default function Spinner() {
-  return (
-    <div className="flex animate-spin items-center justify-center py-4 text-3xl">
-      🍊
-    </div>
-  );
-}
--- apps/zendoblog/src/lib/utils/auth.ts
+++
@@ -1,7 +0,0 @@
-export const publicPaths = ["/", "/sign-in*", "/sign-up*"];
-
-export const isPublicPath = (path: string) => {
-  return publicPaths.find((x) =>
-    path.match(new RegExp(`^${x}$`.replace("*$", "($|/)")))
-  );
-};
