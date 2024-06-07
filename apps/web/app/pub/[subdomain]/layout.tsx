ae88896c305746baae36132fb0e5166fcfec9d9d
Jordi Enric <jordi.err@gmail.com>
2024-06-07 23:14:11 +0200

    Remove footer from Layout component

--- apps/web/app/pub/[subdomain]/layout.tsx
+++ apps/web/app/pub/[subdomain]/layout.tsx
@@ -14,13 +14,6 @@ export default async function Layout({
   return (
     <div>
       <main className="min-h-[90vh]">{children}</main>
-
-      <footer className="mt-12 border-t bg-zinc-50">
-        <div className="mx-auto flex max-w-xl justify-between gap-3  p-2 py-6 font-mono text-sm tracking-tighter text-zinc-500">
-          <Link href="/">{blog?.title}</Link>
-          <Link href="https://www.zenblog.com">Zenblog</Link>
-        </div>
-      </footer>
     </div>
   );
 }
