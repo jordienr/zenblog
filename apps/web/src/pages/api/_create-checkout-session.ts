43d3bfc5604c4c0471ed24d68682fa7e252a48ef
Jordi Enric <jordi.err@gmail.com>
2024-06-12 01:00:30 +0200

    update turbo to 2

--- apps/web/src/pages/api/_create-checkout-session.ts
+++ apps/web/src/pages/api/_create-checkout-session.ts
@@ -8,60 +8,60 @@ import { createClient } from "app/supa";
 
 const handler: NextApiHandler = async (req, res) => {
   return;
-  try {
-    const stripe = createStripeClient();
-    const supa = createClient();
-    const jwt = req.headers.authorization?.replace("Bearer ", "");
-    console.log(jwt);
-    const {
-      data: { user },
-      error,
-    } = await supa.auth.getUser(jwt);
-    const price_id = req.body.price_id;
+  // try {
+  //   const stripe = createStripeClient();
+  //   const supa = createClient();
+  //   const jwt = req.headers.authorization?.replace("Bearer ", "");
+  //   console.log(jwt);
+  //   const {
+  //     data: { user },
+  //     error,
+  //   } = await supa.auth.getUser(jwt);
+  //   const price_id = req.body.price_id;
 
-    console.log(user);
+  //   console.log(user);
 
-    if (!user) {
-      return res.status(401).json({ error: "Unauthorized" });
-    }
+  //   if (!user) {
+  //     return res.status(401).json({ error: "Unauthorized" });
+  //   }
 
-    if (!user.email) {
-      return res.status(400).json({ error: "User email not found" });
-    }
+  //   if (!user.email) {
+  //     return res.status(400).json({ error: "User email not found" });
+  //   }
 
-    if (!price_id) {
-      return res.status(400).json({ error: "Product id not found" });
-    }
+  //   if (!price_id) {
+  //     return res.status(400).json({ error: "Product id not found" });
+  //   }
 
-    const customer = await createOrRetrieveCustomer({
-      userId: user.id,
-      email: user.email,
-    });
+  //   const customer = await createOrRetrieveCustomer({
+  //     userId: user.id,
+  //     email: user.email,
+  //   });
 
-    const session = await stripe.checkout.sessions.create({
-      customer: customer.id,
-      line_items: [
-        {
-          price: price_id,
-          quantity: 1,
-        },
-      ],
-      mode: "subscription",
-      allow_promotion_codes: true,
-      success_url: `${BASE_URL}/account?success=true`,
-      cancel_url: `${BASE_URL}/account?canceled=true`,
-    });
+  //   const session = await stripe.checkout.sessions.create({
+  //     customer: customer.id,
+  //     line_items: [
+  //       {
+  //         price: price_id,
+  //         quantity: 1,
+  //       },
+  //     ],
+  //     mode: "subscription",
+  //     allow_promotion_codes: true,
+  //     success_url: `${BASE_URL}/account?success=true`,
+  //     cancel_url: `${BASE_URL}/account?canceled=true`,
+  //   });
 
-    if (!session.url) {
-      return res.status(500).json({ error: "Error creating session" });
-    }
+  //   if (!session.url) {
+  //     return res.status(500).json({ error: "Error creating session" });
+  //   }
 
-    res.status(200).json({ url: session.url });
-  } catch (error) {
-    console.error(error);
+  //   res.status(200).json({ url: session.url });
+  // } catch (error) {
+  //   console.error(error);
 
-    res.status(500).json({ errorMessage: "Error creating session", error });
-  }
+  //   res.status(500).json({ errorMessage: "Error creating session", error });
+  // }
 };
 
 export default handler;
