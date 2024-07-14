1e31e7dea163fb48d82a8c1ed2d6a91aa207cbca
Jordi Enric <jordi.err@gmail.com>
2024-07-14 13:51:49 +0200

    Comment out method mapping for contract in Page component

--- apps/api/app/docs/page.tsx
+++ apps/api/app/docs/page.tsx
@@ -6,26 +6,26 @@ const methodExamples = {
 } as const;
 
 export default function Page() {
-  const contractString = JSON.stringify(contract, null, 2);
+  // const contractString = JSON.stringify(contract, null, 2);
 
-  const methodKeys = Object.keys(contract);
+  // const methodKeys = Object.keys(contract);
 
-  const methods = methodKeys.map((methodKey) => {
-    const method = contract[methodKey];
-    return (
-      <div key={methodKey}>
-        <h2>{methodKey}</h2>
-        <p>{method.summary}</p>
-        <pre>{methodExamples[methodKey]}</pre>
-      </div>
-    );
-  });
+  // const methods = methodKeys.map((methodKey) => {
+  //   const method = contract[methodKey];
+  //   return (
+  //     <div key={methodKey}>
+  //       <h2>{methodKey}</h2>
+  //       <p>{method.summary}</p>
+  //       <pre>{methodExamples[methodKey]}</pre>
+  //     </div>
+  //   );
+  // });
 
   return (
     <div>
       <h1>Zenblog API Docs</h1>
 
-      {methods.map((m) => m)}
+      {/* {methods.map((m) => m)} */}
     </div>
   );
 }
