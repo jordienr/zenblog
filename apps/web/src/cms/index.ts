bf7251b99826ec9dfc7a769ab4075f778fb8bc03
Jordi Enric <jordi.err@gmail.com>
2024-09-07 17:43:04 +0200

    --wip-- [skip ci]

--- apps/web/src/cms/index.ts
+++ apps/web/src/cms/index.ts
@@ -3,9 +3,9 @@ import { createZenblogClient } from "zenblog";
 const blogId = process.env.ZENBLOG_BLOG_ID || "";
 export const getBlogClient = () => {
   const blog = createZenblogClient({
-    blogId: blogId,
+    accessToken: blogId,
     _url: process.env.NEXT_PUBLIC_API_URL + "/public",
-    debug: true,
+    _debug: true,
   });
 
   return blog;
@@ -13,5 +13,5 @@ export const getBlogClient = () => {
 
 const docsId = process.env.ZENBLOG_DOCS_ID || "";
 export const docs = createZenblogClient({
-  blogId: docsId,
+  accessToken: docsId,
 });
