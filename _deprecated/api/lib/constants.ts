70510f7ef6170093999640c521665f350e0d7837
Jordi Enric <jordi.err@gmail.com>
2024-10-27 00:44:27 +0200

    wipg

--- _deprecated/api/lib/constants.ts
+++
@@ -1,4 +0,0 @@
-import { env } from "./env";
-
-export const API_PATH = "/api/v1";
-export const API_URL = `${env().BASE_API_URL}${API_PATH}`;
--- _deprecated/api/lib/db.ts
+++
@@ -1,7 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-import { env } from "./env";
-
-export const supabase = createClient(
-  env().SUPABASE_URL,
-  env().SUPABASE_SERVICE_ROLE_KEY
-);
