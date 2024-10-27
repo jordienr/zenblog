a035886edabcec31e06326d0cff7f8ca49eeac69
Jordi Enric <jordi.err@gmail.com>
2024-10-27 12:25:06 +0100

    wip

--- apps/web/src/pages/sign-in.tsx
+++ apps/web/src/pages/sign-in.tsx
@@ -80,38 +80,44 @@ export default function SignIn() {
   return (
     <div className="mx-auto my-32 flex max-w-sm flex-col gap-4 px-4">
       <div>
-        <Link className="text-zinc-400" href="/">
+        <Link title="Back to home" className="text-zinc-400" href="/">
           <CornerUpLeft size={18} />
         </Link>
       </div>
-      {loading && (
+      {loading ? (
         <div className="flex h-[600px] items-center justify-center">
           <Loader className="animate-spin text-orange-500" size={24} />
         </div>
+      ) : (
+        <form className="mt-4 flex flex-col gap-2" onSubmit={onSubmit}>
+          <h1 className="text-2xl font-medium">Sign in</h1>
+          <p className="text-slate-500">
+            Don&apos;t have an account?{" "}
+            <Link className="text-slate-800 underline" href="/sign-up">
+              Sign up
+            </Link>
+          </p>
+          <div className="mt-4">
+            <Label htmlFor="email">Email</Label>
+            <Input required type="email" name="email" />
+          </div>
+          <div>
+            <Label
+              htmlFor="password"
+              className="flex items-center justify-between gap-2 py-2"
+            >
+              Password
+              <Link className="text-zinc-500" href="/reset-password">
+                Forgot your password?
+              </Link>
+            </Label>
+            <Input required type="password" name="password" />
+          </div>
+          <div className="mt-2 flex flex-col">
+            <Button type="submit">Sign in</Button>
+          </div>
+        </form>
       )}
-      <form className="mt-4 flex flex-col gap-2" onSubmit={onSubmit}>
-        <h1 className="text-2xl font-medium">Sign in with Password</h1>
-
-        <div className="mt-4">
-          <Label htmlFor="email">Email</Label>
-          <Input required type="email" name="email" />
-        </div>
-        <div>
-          <Label htmlFor="password">Password</Label>
-          <Input required type="password" name="password" />
-        </div>
-        <div className="mt-2 flex flex-col">
-          <Button type="submit">Sign in</Button>
-        </div>
-      </form>
-      <div className="grid gap-4">
-        <Link className="text-zinc-500" href="/reset-password">
-          Forgot your password?
-        </Link>
-        <Link className="text-zinc-500" href="/sign-up">
-          Don&apos;t have an account?
-        </Link>
-      </div>
     </div>
   );
 }
