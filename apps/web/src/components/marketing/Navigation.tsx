712e6497f1dc831f0389c39784e60c3345369601
Jordi Enric <jordi.err@gmail.com>
2024-11-01 21:47:24 +0100

    wip

--- apps/web/src/components/marketing/Navigation.tsx
+++ apps/web/src/components/marketing/Navigation.tsx
@@ -1,51 +1,58 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { ZendoLogo } from "../ZendoLogo";
 import Link from "next/link";
-import { FaTwitter } from "react-icons/fa";
+import { FaBars, FaTwitter } from "react-icons/fa";
 import { Button } from "../ui/button";
 import { useUser } from "@/utils/supabase/browser";
+import { HiOutlineMenu, HiOutlineX } from "react-icons/hi";
 
 type Props = {};
 
 const Navigation = (props: Props) => {
   const user = useUser();
+  const [isOpen, setIsOpen] = useState(false);
+
+  const links = [
+    {
+      label: "Pricing",
+      href: "/pricing",
+    },
+    {
+      label: "Docs",
+      href: "/docs",
+    },
+    {
+      label: "Blog",
+      href: "/blog",
+    },
+    {
+      label: <FaTwitter />,
+      href: "https://twitter.com/zenbloghq",
+      target: "_blank",
+    },
+  ];
 
   return (
     <div className="px-4">
-      <nav className="mx-auto flex w-full max-w-4xl flex-wrap items-center justify-center py-8 md:justify-between">
+      <nav className="mx-auto flex w-full max-w-4xl items-center justify-between py-8">
         <Link href="/" className="">
-          <ZendoLogo className="" size={31} />
+          <ZendoLogo className="" size={32} />
         </Link>
 
-        <div className="flex flex-grow items-center justify-center gap-1 font-medium text-zinc-500 md:justify-end">
-          <Link
-            className="rounded-lg px-2 py-1 hover:text-zinc-800"
-            href="/pricing"
-          >
-            Pricing
-          </Link>
-          <Link
-            className="rounded-lg px-2 py-1 hover:text-zinc-800"
-            href="/docs"
-          >
-            Docs
-          </Link>
-          <Link
-            className="rounded-lg px-2 py-1 hover:text-zinc-800"
-            href="/blog"
-          >
-            Blog
-          </Link>
-          <Link
-            target="_blank"
-            href="https://twitter.com/zenbloghq"
-            className="flex items-center justify-center rounded-full p-2 text-lg transition-all hover:text-zinc-800"
-            title="Follow us on Twitter"
-            aria-label="Follow us on Twitter"
-          >
-            <FaTwitter size="18" />
-          </Link>
+        <div className="flex flex-grow items-center justify-end gap-1 font-medium text-zinc-500 md:justify-end">
+          <div className="hidden items-center gap-2 md:flex">
+            {links.map((link) => (
+              <Link
+                key={link.href}
+                target={link.target}
+                className="rounded-lg px-2 py-1 hover:text-zinc-800"
+                href={link.href}
+              >
+                {link.label}
+              </Link>
+            ))}
+          </div>
 
           {user ? (
             <div className="ml-2 flex">
@@ -55,12 +62,42 @@ const Navigation = (props: Props) => {
             </div>
           ) : (
             <div className="ml-2 flex gap-2">
-              <Button asChild variant={"ghost"}>
+              <Button asChild variant={"ghost"} size="default">
                 <Link href="/sign-in">Login</Link>
               </Button>
-              <Button asChild variant={"default"}>
+              <Button asChild variant={"default"} size="default">
                 <Link href="/sign-up">Sign up</Link>
               </Button>
+            </div>
+          )}
+          <div className="md:hidden">
+            <button
+              className="rounded-lg p-2 hover:bg-zinc-100"
+              onClick={() => setIsOpen(!isOpen)}
+            >
+              <HiOutlineMenu size={24} />
+            </button>
+          </div>
+          {isOpen && (
+            <div className="absolute inset-0 bg-white">
+              <div className="flex h-full flex-col items-center justify-center">
+                {links.map((link) => (
+                  <Link
+                    key={link.href}
+                    target={link.target}
+                    className="rounded-lg px-4 py-4 text-xl hover:text-zinc-800"
+                    href={link.href}
+                  >
+                    {link.label}
+                  </Link>
+                ))}
+                <button
+                  className="mt-4 rounded-lg px-2 py-1 text-xl text-slate-500 hover:text-slate-600"
+                  onClick={() => setIsOpen(false)}
+                >
+                  <HiOutlineX size={24} />
+                </button>
+              </div>
             </div>
           )}
         </div>
