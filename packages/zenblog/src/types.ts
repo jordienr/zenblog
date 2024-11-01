e8a91b4620de353f71061b3ed3d1aa0b26daf050
Jordi Enric <jordi.err@gmail.com>
2024-11-01 16:03:00 +0100

    wip

---
+++ packages/zenblog/src/types.ts
@@ -0,0 +1,5 @@
+import { Category, Post, PostWithContent, Tag } from "@zenblog/types";
+
+// These are exported here so users don't have to install @zenblog/types
+
+export type { Category, Post, PostWithContent, Tag };
