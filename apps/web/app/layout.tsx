83789ac6a42c19b50d141dade879210e8b8fff1e
Jordi Enric <jordi.err@gmail.com>
2024-11-26 18:09:47 +0100

    wip

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -3,7 +3,12 @@ import "./globals.css";
 
 export const metadata = {
   title: "Zenblog blog",
-  description: "An simple headless blogging CMS",
+  description: "A simple headless blogging CMS",
+  openGraph: {
+    title: "Zenblog blog",
+    description: "A simple headless blogging CMS",
+    images: "/static/og.jpg",
+  },
 };
 
 const ibmPlexMono = IBM_Plex_Mono({
@@ -30,7 +35,7 @@ export default function RootLayout({
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1, maximum-scale=1"
-        />
+        />{" "}
       </head>
       <body className={`${ibmPlexMono.variable} ${inter.variable}`}>
         {children}
