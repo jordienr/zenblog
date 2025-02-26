2a85366aa39fe9a03dcfd1b89fa648640958c190
Jordi Enric <jordi.err@gmail.com>
2025-02-26 21:53:33 +0100

    wip

--- apps/web/src/lib/utils.ts
+++ apps/web/src/lib/utils.ts
@@ -1,6 +1,15 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function formatDate(date: string, options?: Intl.DateTimeFormatOptions) {
+  return new Date(date).toLocaleDateString("en-US", {
+    month: "long",
+    day: "numeric",
+    year: "numeric",
+    ...options,
+  });
 }
