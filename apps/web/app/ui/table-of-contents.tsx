7286bdcbff056e035a86bf33a16fb3b677571e50
Jordi Enric <jordi.err@gmail.com>
2025-04-20 16:38:39 +0200

    wip

--- apps/web/app/ui/table-of-contents.tsx
+++ apps/web/app/ui/table-of-contents.tsx
@@ -19,20 +19,23 @@ export function TableOfContents({ items }: Props) {
 
   return (
     <div className="w-full min-w-[240px] max-w-[240px] p-4 text-sm">
-      <h2 className="font-medium">On this page {hash}</h2>
+      <h2 className="font-medium">On this page</h2>
       <ul className="mt-2">
         {items.map((item) => (
           <li key={item.href}>
             <a
               href={`#${item.href}`}
-              className={cn("flex items-center gap-2 p-1", {
+              className={cn("group flex items-center gap-2 p-1", {
                 "text-blue-600": isActive(item.href),
               })}
             >
               <ChevronRight
-                className={cn("h-4 w-4 text-slate-200", {
-                  "text-blue-400": isActive(item.href),
-                })}
+                className={cn(
+                  "h-4 w-4 text-slate-200 transition-colors group-hover:text-blue-400",
+                  {
+                    "text-blue-400": isActive(item.href),
+                  }
+                )}
               />
               {item.title}
             </a>
