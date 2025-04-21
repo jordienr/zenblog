cfc324a3814ee7072e8a1204e74cbf10525c93b5
Jordi Enric <jordi.err@gmail.com>
2025-04-21 12:55:25 +0200

    docs

--- apps/web/app/(docs)/docs/ai-rules/page.tsx
+++ apps/web/app/(docs)/docs/ai-rules/page.tsx
@@ -13,7 +13,7 @@ export default function AiRules() {
         <CodeBlock language="text">
           {`This is the documentation to integrate zenblog into your website.
 Zenblog is a headless CMS that allows you to create and manage your blog. It will host images and videos for you.
-You can install the zenblog package with npm i zenblog to use the typed http client for the API.
+You can install the zenblog package with npm i zenblog to use the typed http client for the API. Make sure the package version is 1.2.0 or higher.
 To integrate zenblog into your website, you just need to make an HTTP request to the API with the Blog ID which you can find in the Zenblog dashboard. The content is returned as an html string. You have to render it to the dom. You can parse it however you want and change the styles and dom elements for your website as you need.
 
 Here is the schema for the API and typescript/javascript client:
