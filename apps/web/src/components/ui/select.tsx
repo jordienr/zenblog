9db2754e0b5833dd2d8cbad5428963c0b6f12100
Jordi Enric <jordi.err@gmail.com>
2026-04-02 16:45:39 +0200

    style: add spacing to select chevron

--- apps/web/src/components/ui/select.tsx
+++ apps/web/src/components/ui/select.tsx
@@ -24,7 +24,7 @@ const SelectTrigger = React.forwardRef<
   >
     {children}
     <SelectPrimitive.Icon asChild>
-      <ChevronDown className="h-4 w-4 opacity-50" />
+      <ChevronDown className="ml-1 h-4 w-4 opacity-50" />
     </SelectPrimitive.Icon>
   </SelectPrimitive.Trigger>
 ));
